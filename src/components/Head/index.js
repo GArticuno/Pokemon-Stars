f8c8b9e1400d12a672bc0149f248a11f17347e14
Mateus Romão <m.romao.ds@gmail.com>
2021-02-07 19:27:59 -0300

    make the ogs

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -5,7 +5,9 @@ export default function index() {
     return (
       <Head>
         <title>Pokemon-stars</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="https://i.pinimg.com/originals/f2/b8/5f/f2b85ff3b6c18f8ba352429d7bdc4de6.jpg?epik=dj0yJnU9UTRVM21SM2g5ME11dTdPSWRLT2JNVjdYZHNMX1JhTmMmcD0wJm49aVRTTGxkbzQ0MUVDVllrcjBnSXVXdyZ0PUFBQUFBR0FQSFhZ" />
+        <meta property="og:title" content="Pokemon-stars" />
+        <meta property="og:description" content="Choose your favorite pokemons and suffer for it" />
       </Head>
     );
   }
